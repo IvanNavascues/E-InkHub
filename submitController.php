375099b2a358996e18be9c56d8f86af247d57fbd
Ivan <navascues.131644@e.unavarra.es>
2024-05-12 17:18:23 +0200

    Prueba de arreglo de post intento 3

--- submitController.php
+++ submitController.php
@@ -7,7 +7,7 @@
 
     if (isset($_POST['numScreen'])) {
         if (isset($_POST['imageBase64']) && isset($_POST['imageHex']) && isset($_POST['imageRed']) && isset($_POST['imageGreen']) && isset($_POST['imageBlue'])) { 
-            $printScreenModule->setScreenImage($_POST['numScreen'],$_POST['imageBase64'],$_POST['imageHex'],$_POST['imageRed'],$_POST['imageGreen'],$_POST['imageBlue']);
+            //$printScreenModule->setScreenImage($_POST['numScreen'],$_POST['imageBase64'],$_POST['imageHex'],$_POST['imageRed'],$_POST['imageGreen'],$_POST['imageBlue']);
             $aResult['status'] = 1;
             //echo '<script type="text/javascript">alert("Pantalla actualizada");</script>';
         }
