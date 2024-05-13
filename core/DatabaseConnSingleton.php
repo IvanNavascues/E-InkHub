e2deda820ad2b4fcb31ec6ba3246fe22956e73f2
Ivan <navascues.131644@e.unavarra.es>
2024-05-13 18:39:09 +0200

    Rama de pruebas, paso de SQLServer a MySQL

--- core/DatabaseConnSingleton.php
+++ core/DatabaseConnSingleton.php
@@ -1,11 +1,7 @@
 <?php
 
 class DatabaseConnSingleton {
-
-    //private static $dbservername = "einkhub.database.windows.net";
-    /*private static $dbuser = "root";
-    private static $dbpass = "";
-    private static $db = "epaper";*/
+/* USAR CON SQL
     private static $conn = null;
     
     public static function getConn(){
@@ -22,18 +18,37 @@ class DatabaseConnSingleton {
         $connectionInfo = array("UID" => "CloudSAbf8ff150", "pwd" => "N0m3c0br315.", "Database" => "epaper", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $serverName = "tcp:einkhub.database.windows.net,1433";
         self::$conn = sqlsrv_connect($serverName, $connectionInfo);
-        /*if (null === self:: $conn ){
+        if (null === self:: $conn ){
             self::$conn = new sqlsrv_connect(self::$dbservername, self::$connectionInfo) or die("Connect failed: %s\n". self::$conn -> error);
             //echo 'created new DB connection';
         }else{
             //echo 'DB connection already existed';
-        }*/
+        }
         return self::$conn;
     }
     public static function closeConn()
     {
         sqlsrv_close(self::$conn);
     }
-
+*/
+    private static $dbhost = "localhost";
+    private static $dbuser = "root";
+    private static $dbpass = "";
+    private static $db = "epaper";
+    private static $conn = null;
+    
+    public static function getConn(){
+        if (null === self:: $conn ){
+            self::$conn = new mysqli(self::$dbhost, self::$dbuser, self::$dbpass, self::$db) or die("Connect failed: %s\n". self::$conn -> error);
+            //echo 'created new DB connection';
+        }else{
+            //echo 'DB connection already existed';
+        }
+        return self::$conn;
+    }
+    public static function closeConn()
+    {
+        self::$conn->close();
+    }
 }
 ?>
--- core/model.php
+++ core/model.php
@@ -93,6 +93,8 @@ class User {
 }
 
 class PrintScreenModule extends Model {
+
+    /* SQL SERVER
     public function getScreens($idUser) {
         $conn = DatabaseConnSingleton::getConn();
 
@@ -126,7 +128,7 @@ class PrintScreenModule extends Model {
             else{
                 sqlsrv_rollback($conn);
             }
-            /* Free statement and connection resources. */
+            // Free statement and connection resources. 
             sqlsrv_free_stmt( $stmt);
 
         } catch (Exception $e){
@@ -156,10 +158,49 @@ class PrintScreenModule extends Model {
         catch(Exception $e) {
             //echo("Error!");
         }
+    }*/
+
+    public function getScreens($idUser) {
+        $conn = DatabaseConnSingleton::getConn();
+
+        $screenList = array();
+        $query = "SELECT screens.* FROM screens JOIN userscreens ON screens.id = userscreens.idScreen WHERE userscreens.idUser = ".$idUser;
+
+        if ($res = $conn->query("$query")) {
+            while ($row = $res->fetch_assoc()) {
+                array_push($screenList, new Screen($row['id'],$row['name'],$row['width'],$row['height'],$row['color'],$row['imageBase64'],$row['imageHex'],$row['imageRed'],$row['imageGreen'],$row['imageBlue']));
+            }
+            $res->free();
+            return $screenList;
+        }
+    }
+
+    public function setScreenImage($numScreen,$imageBase64,$imageHex,$imageRed,$imageGreen,$imageBlue) {
+        $conn = DatabaseConnSingleton::getConn();
+
+        $query = "UPDATE screens SET imageBase64 = '".$imageBase64."',imageHex = '".$imageHex."',imageRed = '".$imageRed."',imageGreen = '".$imageGreen."',imageBlue = '".$imageBlue."' WHERE id = '".$numScreen."'";
+
+        return $conn->query("$query");
+    }
+
+    public function getScreenPrintable($numScreen) {
+        $conn = DatabaseConnSingleton::getConn();
+
+        $query = "SELECT * FROM screens WHERE id = '".$numScreen."'";
+
+        if ($res = $conn->query("$query")) {
+            $row = $res->fetch_assoc(); 
+            $res->free();
+            return new Screen($row['id'],$row['name'],$row['width'],$row['height'],$row['color'],$row['imageBase64'],$row['imageHex'],$row['imageRed'],$row['imageGreen'],$row['imageBlue']);
+        }
+        else
+            return null;
     }
 }
 
 class LoginScreenModule extends Model {
+
+    /* SQL SERVER
     public function checkLogin($username,$password) {
         try
         {
@@ -182,6 +223,22 @@ class LoginScreenModule extends Model {
         catch(Exception $e){
             //echo("Error!");
         }
+    }*/
+
+    public function checkLogin($username,$password) {
+        $conn = DatabaseConnSingleton::getConn();
+
+        $query = "SELECT * FROM users WHERE username = '".$username."' AND password = '".$password."'";
+
+        if ($res = $conn->query("$query")) {
+            $row = $res->fetch_assoc(); 
+            if (!empty($row)) {
+                $res->free();
+                return new User($row['id'],$row['username'],$row['password']);
+            }
+        }
+        
+        return null;
     }
 }
 
